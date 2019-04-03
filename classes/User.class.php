4f5aaba32d0ec169cdadfcd0bb6484456c8d0a11
AqsaIntizar <aqsatje@live.be>
2019-04-03 14:55:17 +0200

    added a class folder

---
+++ classes/User.class.php
@@ -0,0 +1,111 @@
+<?php
+
+class User{
+    private $email;
+    private $fullname;
+    private $username;
+    private $password;
+    private $password_confirm;
+
+    
+
+    /**
+     * Get the value of email
+     */ 
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @return  self
+     */ 
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of fullname
+     */ 
+    public function getFullname()
+    {
+        return $this->fullname;
+    }
+
+    /**
+     * Set the value of fullname
+     *
+     * @return  self
+     */ 
+    public function setFullname($fullname)
+    {
+        $this->fullname = $fullname;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of username
+     */ 
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set the value of username
+     *
+     * @return  self
+     */ 
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of password
+     */ 
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set the value of password
+     *
+     * @return  self
+     */ 
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of password_confirm
+     */ 
+    public function getPassword_confirm()
+    {
+        return $this->password_confirm;
+    }
+
+    /**
+     * Set the value of password_confirm
+     *
+     * @return  self
+     */ 
+    public function setPassword_confirm($password_confirm)
+    {
+        $this->password_confirm = $password_confirm;
+
+        return $this;
+    }
+}
