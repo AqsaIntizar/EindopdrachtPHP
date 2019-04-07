9145cd6d58277384e205dcb1584ca2b2e6219074
Wesley Wijsen <wesleywijsen@hotmail.com>
2019-04-07 16:01:16 +0200

    added nav to index-page
    ik heb een navigatie toegevoegd, gestyled en dynamisch gemaakt

---
+++ nav.inc.php
@@ -0,0 +1,18 @@
+<div class="logo"></div>
+<nav>
+    <ul class="navFlex">
+        <li><a href="#" class="navLink">Link 1</a></li>
+        <li><a href="#" class="navLink">Link 2</a></li>
+        <li><a href="#" class="navLink">Link 3</a></li>
+        <li><a href="#" class="navLink">Link 4</a></li>
+        <li>
+            <div class="dropdown">
+                <img src="https://fakeimg.pl/50x50/" class="dropbtn">
+                <div class="dropdown-content">
+                    <a href="#">Instellingen</a>
+                    <a href="#">Log out</a>
+                </div>
+            </div>
+        </li>
+    </ul>
+</nav>
