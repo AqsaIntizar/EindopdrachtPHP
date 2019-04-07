8ace23db916e1968321aa3f88106dd9c8e3a21e2
Wesley Wijsen <wesleywijsen@hotmail.com>
2019-04-07 18:07:35 +0200

    fixed write in db
    kan nu paden wegschrijven in de database, hardcoded want de session werkt nog niet goed

--- upload.php
+++ upload.php
@@ -5,6 +5,7 @@
         //request aanwezig, kijken of er files zijn om up te loaden
         if( isset($_FILES['upload']) ){
             $fileName = $_FILES['upload']['name'];
+            $fileName = preg_replace('/\s+/', '_', $fileName);
             $fileType = $_FILES['upload']['type'];
             $fileTempName = $_FILES['upload']['tmp_name'];
             $fileSize = $_FILES['upload']['size'];
@@ -14,9 +15,11 @@
 
             //uploaden
             if( move_uploaded_file($fileTempName, $targetDir.$fileName) ){
+                $fullPath =  $targetDir.$fileName;
                 //connect db
                 $conn = Db::getInstance();
-                $stmnt = $conn->prepare('update users set img_dir = "image/profilePics/me.jpg" where email = "wesleywijsen@hotmail.com"');
+                $stmnt = $conn->prepare('update users set img_dir = :dir where email = "wesleywijsen@hotmail.com"');
+                $stmnt->bindParam(":dir", $fullPath);
                 $result = $stmnt->execute();
                 return $result;
             }else{
