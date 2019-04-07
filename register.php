d82f27754b61a24ab547b92cfe0fe80eedce79db
AqsaIntizar <aqsatje@live.be>
2019-04-07 11:38:53 +0200

    deleted the full name and added lastname en firstname in the form

--- register.php
+++ register.php
@@ -2,48 +2,51 @@
 require_once("classes/User.class.php");
 
 if(!empty($_POST)){
-    $user = new User();
-    $user->setFullname($_POST['fullname']);
-    $user->setUsername($_POST['username']);
+	$user = new User();
 	$user->setEmail($_POST['email']);
+	$user->setFirstname($_POST['firstname']);
+	$user->setLastname($_POST['lastname']);
+	$user->setUsername($_POST['username']);
 	$user->setPassword($_POST['password']);
 	$user->setPasswordConfirmation($_POST['password_confirmation']);
+
+	if($user->register()){
+		session_start();
+		$_SESSION['email'] = $user->getEmail();
+		header('Location: index.php');
+	}
 }
+
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>Eindopdracht</title>
+  <title>IMDFlix</title>
   <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <form action="" method="post">
-        
-		<div class="form__field">
-			<label for="fullname">Full name</label>
-			<input type="text" id="full_name" name="fullname">
-		</div>
-        <div class="form__field">
-			<label for="username">Username</label>
-			<input type="text" id="username" name="username">
-		</div>
-        <div class="form__field">
-			<label for="email">Email</label>
-			<input type="text" id="email" name="email">
-		</div>
-		<div class="form__field">
-			<label for="password">Password</label>
-			<input type="password" id="password" name="password">
-		</div>
-        <div class="form__field">
-		    <label for="password_confirmation">Confirm your password</label>
-			<input type="password" id="password_confirmation" name="password_confirmation">
-		</div>
+	<form action="" method="post">
+		<h2>Sign up for an account</h2>
+				
+		<label for="firstanme">Firstname</label>
+		<input type="text" id="firstname" name="firstname">
+			
+		<label for="lastname">Lastname</label>
+		<input type="text" id="lastname" name="lastname">
 
-		<div class="form__field">
-			<input type="submit" value="Registreer">
-		</div>
+		<label for="username">Username</label>
+		<input type="text" id="username" name="username">
+
+		<label for="email">Email</label>
+		<input type="text" id="email" name="email">
+
+		<label for="password">Password</label>
+		<input type="password" id="password" name="password">
+
+		<label for="password_confirmation">Confirm your password</label>
+		<input type="password" id="password_confirmation" name="password_confirmation">
+
+		<input type="submit" value="Sign me up!" class="btn btn--primary">
 	</form>
-
 </body>
 </html>
