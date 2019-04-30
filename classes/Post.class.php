d42f8465bec46db6d3b4ca1f4300d7138ec4593a
AqsaIntizar <aqsatje@live.be>
2019-04-30 16:10:51 +0200

    modiefied post class

--- classes/Post.class.php
+++ classes/Post.class.php
@@ -8,13 +8,13 @@ class Post
         // fetch all records from the database and return them as objects of this __CLASS__ (Post)
         return $result->fetchAll(PDO::FETCH_CLASS, __CLASS__);
 
-
     }
 
     public function getLikes(){
         $conn = Db::getInstance();
         $statement = $conn->prepare("select count(*) as count from likes where post_id = :postid");
-        $statement->bindValue(":postid", $this->id);
+        $statement->bindValue(":postid", $this->Id);
+        $statement->bindValue(':postid', $this->id, PDO::PARAM_INT);
         $statement->execute();
         $result = $statement->fetch(PDO::FETCH_ASSOC);
         return $result['count'];
