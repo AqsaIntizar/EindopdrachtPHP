dd8d12d232cfd8f2372e72d0a0148475311b5919
AqsaIntizar <aqsatje@live.be>
2019-05-01 14:57:04 +0200

    added conditions

--- classes/User.class.php
+++ classes/User.class.php
@@ -28,9 +28,14 @@
          */
         public function setEmail($email)
         {
-                $this->email = $email;
+            if( empty($email) ){
+                throw new Exception("Email cannot be empty.");
+            }
 
-                return $this;
+            // todo: valid email ? -> filter_var()
+
+            $this->email = $email;
+            return $this;
         }
 
         /**
@@ -157,7 +162,6 @@
                 return $this;
         }
         public function register(){
-            $password = Security::hash($this->password);
   
               try{
                 
@@ -168,13 +172,42 @@
                 $statement->bindParam(":firstname", $this->firstname);
                 $statement->bindParam(":lastname", $this->lastname);
                 $statement->bindParam(":username", $this->username);
-                $statement->bindParam(":password", $password);
+                $hash = password_hash($this->password, PASSWORD_BCRYPT);
+                $statement->bindParam(":password", $hash);
                 $result = $statement->execute();
                 return $result;
               }catch(Throwable $t){
                   return false;
               }
         }
+        public function login() {
+                if(!isset($_SESSION)) { 
+                    session_start(); 
+                } 
+                $_SESSION['username'] = $this->email;
+                header('Location: index.php');
+            }
+        public static function findByEmail($email){
+                $conn = Db::getInstance();
+                $statement = $conn->prepare("select * from users where email = :email limit 1");
+                $statement->bindValue(":email", $email);
+                $statement->execute();
+                return $statement->fetch(PDO::FETCH_ASSOC);
+            }
+    
+            /* Check if a user exists based on a give email address */
+            public static function isAccountAvailable($email){
+                $u = self::findByEmail($email);
+                
+                // PDO returns false if no records are found so let's check for that
+                if($u == false){
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+    
+        
         public static function saveDiscription($userName){
                 //echo "test";
                 $myDiscr = htmlspecialchars($_POST['myDiscr'], ENT_QUOTES);
