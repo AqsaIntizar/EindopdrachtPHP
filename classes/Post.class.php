91c5b99c5217124afc577fdfafd5957d92221e32
Wesley <wesleywijsen@hotmail.com>
2019-05-09 16:26:32 +0200

    made userposts on detail page

--- classes/Post.class.php
+++ classes/Post.class.php
@@ -2,130 +2,113 @@
 
 class Post
 {
-	public static function getAll(){
-
-        if(!isset($_GET['showitems'])){
+    public static function getAll()
+    {
+        if (!isset($_GET['showitems'])) {
             $itemCount = 20;
         } else {
-            $itemCount = (int)$_GET['showitems'];
+            $itemCount = (int) $_GET['showitems'];
             //echo $itemCount;
         }
-       
-        
+
         $conn = Db::getInstance();
         $stmnt = $conn->prepare('select posts.id, user_id, post_img_dir,post_description,username,date_created from posts, users where posts.user_id = users.id ORDER BY id DESC LIMIT :itemCount');
         $stmnt->bindValue(':itemCount', $itemCount, PDO::PARAM_INT);
         $stmnt->execute();
-        return $result= $stmnt->fetchAll(PDO::FETCH_ASSOC);
+
+        return $result = $stmnt->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    public static function getSearchResults(){
-        
-        if(!isset($_GET['showitems'])){
+    public static function getAllById($usedId)
+    {
+        if (!isset($_GET['showitems'])) {
             $itemCount = 20;
         } else {
-            $itemCount = (int)$_GET['showitems'];
+            $itemCount = (int) $_GET['showitems'];
             //echo $itemCount;
         }
-       
-        $search = '%' . $_GET["searchResult"] . '%';
+
+        $conn = Db::getInstance();
+        $stmnt = $conn->prepare('select posts.id, user_id, post_img_dir,post_description,username,date_created from posts, users where posts.user_id = users.id and posts.user_id = :id ORDER BY id DESC LIMIT :itemCount');
+        $stmnt->bindValue(':id', $usedId);
+        $stmnt->bindValue(':itemCount', $itemCount, PDO::PARAM_INT);
+        $stmnt->execute();
+
+        return $result = $stmnt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public static function getSearchResults()
+    {
+        if (!isset($_GET['showitems'])) {
+            $itemCount = 20;
+        } else {
+            $itemCount = (int) $_GET['showitems'];
+            //echo $itemCount;
+        }
+
+        $search = '%'.$_GET['searchResult'].'%';
 
         $conn = Db::getInstance();
         $stmnt = $conn->prepare('select posts.id, user_id, post_img_dir,post_description,username from posts, users where posts.user_id = users.id AND post_description LIKE :hashtag ORDER BY id DESC LIMIT :itemCount ');
         $stmnt->bindValue(':itemCount', $itemCount, PDO::PARAM_INT);
-        $stmnt->bindParam(":hashtag", $search);
+        $stmnt->bindParam(':hashtag', $search);
         $stmnt->execute();
-        return $result= $stmnt->fetchAll(PDO::FETCH_ASSOC);
+
+        return $result = $stmnt->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    public static function timeAgo($timestamp){
-        date_default_timezone_set("Europe/Brussels");         
-        $time_ago        = strtotime($timestamp);
-        $current_time    = time();
+    public static function timeAgo($timestamp)
+    {
+        date_default_timezone_set('Europe/Brussels');
+        $time_ago = strtotime($timestamp);
+        $current_time = time();
         $time_difference = $current_time - $time_ago;
-        $seconds         = $time_difference;
-        
-        $minutes = round($seconds / 60); // value 60 is seconds  
-        $hours   = round($seconds / 3600); //value 3600 is 60 minutes * 60 sec  
-        $days    = round($seconds / 86400); //86400 = 24 * 60 * 60;  
-        $weeks   = round($seconds / 604800); // 7*24*60*60;  
-        $months  = round($seconds / 2629440); //((365+365+365+365+366)/5/12)*24*60*60  
-        $years   = round($seconds / 31553280); //(365+365+365+365+366)/5 * 24 * 60 * 60
-                        
-        if ($seconds <= 60){
+        $seconds = $time_difference;
 
-            return "Zonet";
+        $minutes = round($seconds / 60); // value 60 is seconds
+        $hours = round($seconds / 3600); //value 3600 is 60 minutes * 60 sec
+        $days = round($seconds / 86400); //86400 = 24 * 60 * 60;
+        $weeks = round($seconds / 604800); // 7*24*60*60;
+        $months = round($seconds / 2629440); //((365+365+365+365+366)/5/12)*24*60*60
+        $years = round($seconds / 31553280); //(365+365+365+365+366)/5 * 24 * 60 * 60
 
-        } else if ($minutes <= 60){
-
-            if ($minutes == 1){
-
-            return "1 minuut geleden";
-
+        if ($seconds <= 60) {
+            return 'Zonet';
+        } elseif ($minutes <= 60) {
+            if ($minutes == 1) {
+                return '1 minuut geleden';
             } else {
-
-            return "$minutes minuten geleden";
-
+                return "$minutes minuten geleden";
             }
-
-        } else if ($hours <= 24){
-
-            if ($hours == 1){
-
-            return "Een uur geleden";
-
+        } elseif ($hours <= 24) {
+            if ($hours == 1) {
+                return 'Een uur geleden';
             } else {
-
-            return "$hours uur geleden";
-
+                return "$hours uur geleden";
             }
-
-        } else if ($days <= 7){
-
-            if ($days == 1){
-
-            return "Gisteren";
-
+        } elseif ($days <= 7) {
+            if ($days == 1) {
+                return 'Gisteren';
             } else {
-
-            return "$days dagen geleden";
-
+                return "$days dagen geleden";
             }
-
-        } else if ($weeks <= 4.3){
-
-            if ($weeks == 1){
-
-            return "Een week geleden";
-
+        } elseif ($weeks <= 4.3) {
+            if ($weeks == 1) {
+                return 'Een week geleden';
             } else {
-
-            return "$weeks weken geleden";
-
+                return "$weeks weken geleden";
             }
-
-        } else if ($months <= 12){
-
-            if ($months == 1){
-
-            return "Een maand geleden";
-
+        } elseif ($months <= 12) {
+            if ($months == 1) {
+                return 'Een maand geleden';
             } else {
-
-            return "$months maanden geleden";
-
+                return "$months maanden geleden";
             }
-
         } else {
-            
-            if ($years == 1){
-
-            return "Een jaar geleden";
-
+            if ($years == 1) {
+                return 'Een jaar geleden';
             } else {
-
-            return "$years jaren geleden";
-
+                return "$years jaren geleden";
             }
         }
     }
---
+++ profileDetails.php
@@ -0,0 +1,89 @@
+<?php
+    require_once 'bootstrap.php';
+
+    //$userName = $_SESSION['UserName'];
+    if (isset($_SESSION['User'])) {
+        //logged in user
+        //echo "😎";
+    } else {
+        //no logged in user
+        header('Location: login.php');
+    }
+    $id = $_GET['id'];
+    $result = Post::getAllById($id);
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>includeFood - Profile Details</title>
+    
+</head>
+<body>
+    <header>
+        <?php require_once 'nav.inc.php'; ?>
+    </header>
+    <div class="container details">
+        <div class="sideNav">
+            <img src="" alt="Profile Picture">
+            <span class="name">naam</span>
+            <a href="#">Follow</a>
+            <hr>
+            <a href="#">Posts</a>
+            <a href="#">Info</a>
+        </div>
+        <div class="content--details">
+
+            <?php $counter = 0; ?>
+                <!-- start lus -->
+                <?php foreach ($result as $r): ?>
+   
+                <div class="post" id="<?php echo $r['id']; ?>" data-id="<?php echo $r['id']; ?>">
+   
+                <img class="postImg" src="<?php echo $r['post_img_dir']; ?>" alt="">
+                <p class="description">
+                    <?php
+                        $hashtag = $r['post_description'];
+                        $linked_string = preg_replace("/#([^\s]+)/", '<a href="search.php?searchResult=$1">#$1</a>', $hashtag);
+                        echo $linked_string;
+                    ?>
+                </p>
+                <p><strong><?php echo Post::timeAgo($r['date_created']); ?></strong></p>
+                <a href="profileDetails.php?id=<?php echo $r['user_id']; ?>" class="post__item"><span class="infoBlock"><strong><?php echo $r['username']; ?></strong></span></a>
+                <!--<p><strong><a href="#"><?php //echo $r['username'];?></a> </strong></p>-->
+                
+                
+                <form method="post" action="">
+                    <input type="text" placeholder="Comment Here" class="comment" name="comment"/>
+                    <input type="submit" value="Post comment" class="btnSub" />
+
+                    <ul class="comments">
+                        <?php
+                            //echo $r['id'];
+                            $comments = Comment::getAll($r['id']);
+                            if (is_array($comments) || is_object($comments)) {
+                                foreach ($comments as $c) {
+                                    echo '<li>'.htmlspecialchars($c['text'], ENT_QUOTES).'</li>';
+                                }
+                            }
+
+                        ?>
+                    </ul>
+                </form>
+            </div>
+
+            <div class="fullView" id="full-<?php echo $r['id']; ?>" data-full-id="full-<?php echo $r['id']; ?>">
+                <span class="x">X</span>
+                <img src="<?php echo $r['post_img_dir']; ?>" alt="">
+            </div>
+            <?php ++$counter; ?>
+            <?php endforeach; ?>
+
+
+            <a href="index.php?showitems=<?php echo $counter + 3; ?>' class="load">Load More</a>
+        </div>
+    </div>
+</body>
+</html>
