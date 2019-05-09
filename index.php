32cde95194ab18c304ef6f36e7cfbb7204c5b6c0
Sam <samlinck@outlook.com>
2019-05-09 18:02:47 +0200

    fixed merge conflict

--- index.php
+++ index.php
@@ -34,17 +34,6 @@
    
     <div class="post" id="<?php echo $r['id']; ?>" data-id="<?php echo $r['id']; ?>">
     
-<<<<<<< HEAD
-        <img class="postImg" src="images/posts/<?php echo $r['post_img_dir']; ?>" alt="">
-        <p class="description"><?php  $hashtag = $r['post_description'];
-    $linked_string = preg_replace("/#([^\s]+)/", "<a href=\"search.php?q=$1\">#$1</a>", $hashtag);
-    echo $linked_string ?></p>
-||||||| merged common ancestors
-        <img class="postImg" src="<?php echo $r['post_img_dir']; ?>" alt="">
-        <p class="description"><?php  $hashtag = $r['post_description'];
-    $linked_string = preg_replace("/#([^\s]+)/", "<a href=\"search.php?q=$1\">#$1</a>", $hashtag);
-    echo $linked_string ?></p>
-=======
         <img class="postImg" src="<?php echo $r['post_img_dir']; ?>" alt="">
         <p class="description">
             <?php  
@@ -54,7 +43,6 @@
             ?>
         </p>
         <p><strong><?php echo Post::timeAgo($r['date_created']); ?></strong></p>
->>>>>>> 35a5f066df2f1b3f302b92bde9e0e88374cfd9d6
         <p><strong><?php echo $r['username']; ?></strong></p>
         
         
