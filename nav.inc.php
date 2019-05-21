99ff1a7bab52cb44eb6b7b05ef0b841ed4bce741
Sam <samlinck@outlook.com>
2019-05-22 00:47:55 +0200

    styled nav and added logo

--- nav.inc.php
+++ nav.inc.php
@@ -5,15 +5,15 @@
         <li><a href="follows.php" class="navLink">Follows</a></li>
         <li><a href="mostLiked.php" class="navLink">Most liked</a></li>
         <li><a href="hashtagFollow.php" class="navLink">Hashtags</a></li>
-        <form action="search.php" method="get">
+        <form class="search" action="search.php" method="get">
             <input type="text" name="searchResult" placeholder="Search...">
         </form>
         <li>
             <div class="dropdown">
                 <img src="images/profilePics/<?php echo $_SESSION['user']['img_dir']; ?>" class="dropbtn">
                 <div class="dropdown-content">
-                    <a href="settings.php">Instellingen</a>
-                    <a href="logout.php">Hi <?php echo $_SESSION['user']['username']; ?>, log out?</a>
+                    <a id="drop-black" href="settings.php">Instellingen</a>
+                    <a id="drop-black" href="logout.php">Hi <?php echo $_SESSION['user']['username']; ?>, log out?</a>
                 </div>
             </div>
         </li>
