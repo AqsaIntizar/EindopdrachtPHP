07ed987f4073fd3d758cbe7e48d7c194158d9ef5
AqsaIntizar <aqsatje@live.be>
2019-05-22 21:44:29 +0200

    removed hashtag link

--- nav.inc.php
+++ nav.inc.php
@@ -4,7 +4,6 @@
         <li><a href="index.php" class="navLink">Home</a></li>
         <li><a href="follows.php" class="navLink">Follows</a></li>
         <li><a href="mostLiked.php" class="navLink">Most liked</a></li>
-        <li><a href="hashtagFollow.php" class="navLink">Hashtags</a></li>
         <form class="search" action="search.php" method="get">
             <input type="text" name="searchResult" placeholder="Search..." class="search">
         </form>
